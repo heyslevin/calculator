a86a234dcda56d530f585a5f7b86888dda3269d7
memellaguno <memellaguno@gmail.com>
2020-03-18 09:00:12 -0600

    Clear button works, started working on operator buttons

--- script.js
+++ script.js
@@ -1,27 +1,51 @@
 
+let a = 0;
+let b = 0;
+let operator = "";
 
-
-//Display
+//Display Selector
 let disp = document.querySelector('[data-key="display"]')
 
 
-//Press Down
+//Button functions
 
 function pressdown(e) {
 
 	disp.textContent += this.dataset.key;
 };
 
+function cleanup() {
+	disp.textContent = "";
+}
+
+function opcap() {
+	operator = this.dataset.key;
+	
+
+}
+
+
+
 //Button Selectors
 
-const button = document.querySelectorAll(".button");
+const button = document.querySelectorAll(".numbutton");
+const clear = document.querySelector(".clear");
+const opbutton = document.querySelectorAll(".opbutton")
 
+
+//Event Listener for regular buttons
+
+clear.addEventListener('click',cleanup)
 
 for (let i = 0; i < button.length; i++) {
 	button[i].addEventListener('click',pressdown);
 }
 
+//Event Listener for operator buttons
 
+for (let i = 0; i < opbutton.length; i++) {
+	opbutton[i].addEventListener('click',opcap)
+}
 
 
 function add(a,b) {
